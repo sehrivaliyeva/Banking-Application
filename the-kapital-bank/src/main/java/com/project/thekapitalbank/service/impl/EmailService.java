32849fc9a02bfecf9cd5c35c19a2ed72fc5277f8
sehrivaliyeva <114057052+sehrivaliyeva@users.noreply.github.com>
2024-04-20 02:24:10 +0400

    email message sending

---
+++ the-kapital-bank/src/main/java/com/project/thekapitalbank/service/impl/EmailService.java
@@ -0,0 +1,7 @@
+package com.project.thekapitalbank.service.impl;
+
+import com.project.thekapitalbank.dto.EmailDetails;
+
+public interface EmailService {
+    void sendEmailAlert(EmailDetails emailDetails);
+}
--- the-kapital-bank/src/main/java/com/project/thekapitalbank/service/impl/UserServiceImpl.java
+++ the-kapital-bank/src/main/java/com/project/thekapitalbank/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.project.thekapitalbank.service.impl;
 
 import com.project.thekapitalbank.dto.AccountInfo;
 import com.project.thekapitalbank.dto.BankResponse;
+import com.project.thekapitalbank.dto.EmailDetails;
 import com.project.thekapitalbank.dto.UserRequest;
 import com.project.thekapitalbank.entity.User;
 import com.project.thekapitalbank.repository.UserRepository;
@@ -15,6 +16,8 @@ import java.math.BigDecimal;
 public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    EmailService emailService;
 
     @Override
     public BankResponse createAccount(UserRequest userRequest) {
@@ -40,6 +43,16 @@ public class UserServiceImpl implements UserService {
                 .status("ACTIVE")
                 .build();
         User savedUser = userRepository.save(newUser);
+        // Send email alert
+        EmailDetails emailDetails = EmailDetails.builder()
+                .recipient(savedUser.getEmail())
+                .subject("ACCOUNT CREATION")
+                .messageBody("Congratulations! Your Account Has Been Successfully Created.\nYour Account Details: \n" +
+                        "Account Name: " + savedUser.getFirstName() + " " + savedUser.getLastName() + " " + savedUser.getOtherName() + "\nAccount Number: "
+                        + " " + savedUser.getAccountNumber())
+                .build();
+        emailService.sendEmailAlert(emailDetails);
+
         return BankResponse.builder()
                 .responseCode(AccountUtils.ACCOUNT_CREATION_SUCCESS)
                 .responseMessage(AccountUtils.ACCOUNT_CREATION_MESSAGE)
